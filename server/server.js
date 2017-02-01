e4c8472a429cc1c2d135e03b84ffabb50ca50f35
Andrea <bigroger1@gmail.com>
2017-02-01 13:17:53 +0000

    Init commit

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const express = require('express')
+
+var publicPath = path.join(__dirname, '../public');
+var app = express();
+
+app.use(express.static(publicPath));
+app.listen(3000, () => {
+  console.log('Server is up');
+})
